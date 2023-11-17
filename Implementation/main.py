5d840a00ad8ddb319d4dbb916d2b4cfedce8e490
MateuszFerenc <mateoferenc@gmail.com>
2023-11-17 16:43:09 +0100

    Added primary simulator tabs (frames)

--- Implementation/main.py
+++ Implementation/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import constants
+import types
+import re
 
 
 def get_center(parent, axis, val):
@@ -21,6 +23,105 @@ class MainWindow(tk.Tk):
         self.resizable(False, False)
         self.configure(bg=constants.window_background_color)
 
+        self.notebook = ttk.Notebook(self, takefocus=False, width=constants.window_width, height=constants.window_height)
+        self.notebook.place(rely=0, relwidth=1)
+
+        self.notebook_frames = []
+        for win_frame in sim_frames:
+            frame = win_frame(parent=self)
+            frame.pack(expand=False)
+            self.notebook.add(frame, text=frame.name)
+            self.notebook_frames.append(frame)
+            
+
+class SimulationFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Simulation"
+        self.long_name = "SimulationFrame"
+        self.parent = parent
+        label = ttk.Label(self, text="Tab 0")
+        label.pack()
+        # self.create_ui()
+        # self.content_update()
+
+    def create_ui(self):
+        pass
+
+    def content_update(self):
+        pass
+
+class InputsFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Inputs"
+        self.long_name = "InputsFrame"
+        self.parent = parent
+        label = ttk.Label(self, text="Tab 1")
+        label.pack()
+        # self.create_ui()
+        # self.content_update()
+
+    def create_ui(self):
+        pass
+
+    def content_update(self):
+        pass
+
+class ControlFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Control"
+        self.long_name = "ControlFrame"
+        self.parent = parent
+        label = ttk.Label(self, text="Tab 2")
+        label.pack()
+        # self.create_ui()
+        # self.content_update()
+
+    def create_ui(self):
+        pass
+
+    def content_update(self):
+        pass
+
+class OutputsFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Outputs"
+        self.long_name = "OutputsFrame"
+        self.parent = parent
+        label = ttk.Label(self, text="Tab 3")
+        label.pack()
+        # self.create_ui()
+        # self.content_update()
+
+    def create_ui(self):
+        pass
+
+    def content_update(self):
+        pass
+
+class GraphsFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Graphs"
+        self.long_name = "GraphsFrame"
+        self.parent = parent
+        label = ttk.Label(self, text="Tab 4")
+        label.pack()
+        # self.create_ui()
+        # self.content_update()
+
+    def create_ui(self):
+        pass
+
+    def content_update(self):
+        pass
+
+
+sim_frames = list(globals()[c] for c, x in globals().copy().items() if re.match('.*Frame$', c))
+# create list of class types defined in this scope
 
 if __name__ == "__main__":
     main = MainWindow()
