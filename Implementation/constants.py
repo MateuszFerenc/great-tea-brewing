4fb727109c6ddc092123028a838d37de67875d33
Jula434 <JuliaBanaszak2003@gmail.com>
2024-01-11 21:45:05 +0100

    image

--- Implementation/constants.py
+++ Implementation/constants.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     exit(-1)
 
-window_width = 800          #  width of the window, can be changed if desired, be aware of layout misplace wheen changing width or height value
-window_height = 500         #  height of the window
+window_width = 1400          #  width of the window, can be changed if desired, be aware of layout misplace wheen changing width or height value
+window_height = 800         #  height of the window
 window_background_color = "#BBBBBB"     # some random background color, can be changed if desired
 simulation_tick = 10        # ms, amount of time for one tick of simulation
 simulation_default_sampling = 5   # samples per second, 1000 ms / sampling = one sample time (ms) [1000ms/5=200ms => 200ms/10ms=20 simulation ticks per sample]
